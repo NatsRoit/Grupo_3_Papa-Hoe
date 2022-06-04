e957292319d87b76764e02582e91c334c007e7df
NatsRoit <nats.roitman@gmail.com>
2022-06-04 12:58:38 +0200

    Cambio dependencies Package.json

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ app.use( express.static(path.resolve(__dirname, './public')));
 
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'views', 'home.html'))
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 app.get('/register', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'views', 'register.html'))
+    res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
 app.get('/login', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'views', 'login.html'))
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
 });
 app.get('/productCart', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'views', 'productCart.html'))
+    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
 });
 app.get('/productDetail', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'views', 'productDetail.html'))
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'))
 });
 
 app.listen(process.env.PORT || 3030, () => {
