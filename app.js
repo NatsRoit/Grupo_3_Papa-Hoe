ab8f1cac5a216c39ab5eee15aa3a276f75ffa3f6
NatsRoit <nats.roitman@gmail.com>
2022-06-04 11:24:30 +0200

    Cambio nombre index --> home

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use( express.static(path.resolve(__dirname, './public')));
 
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
 });
 app.get('/register', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
