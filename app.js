c103c92814f0891d48fb00c38eafddf8138b8f92
NatsRoit <nats.roitman@gmail.com>
2022-06-06 10:26:04 +0200

    Cambio file app.get(/)

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const app = express();
 app.use( express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+    res.sendFile('home.html', {root: '/'});
 });
+    
 app.get('/register', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'))
 });
