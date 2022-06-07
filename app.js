cbab217ed75fe617aba7c29c4f831e878f0f8df5
alfre2020 <alfredobuzzin@outlook.com>
2022-06-07 18:47:27 -0300

    .

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
+const express = require('express');
+const path = require('path');
 
-const express = require('express')
-const path = require('path')
 const app = express();
-const publicPath = path.join(__dirname, './public')
-app.use( express.static(publicPath ))
 
+app.use( express.static(path.resolve(__dirname, './public')));
 
-app.listen(process.env.PORT || 3030, function() {
-    console.log('Servidor corriendo')
-})
+app.listen(process.env.PORT || 3030, function () {
+    console.log('Servidor corriendo');
+});
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
