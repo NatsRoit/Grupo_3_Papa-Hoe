df8cc772fc062c266d2ce6919cb816f4ce75a763
NatsRoit <nats.roitman@gmail.com>
2022-06-17 03:53:34 +0200

    PARTIALS: Head, Footer y Header

--- app.js
+++ app.js
@@ -9,33 +9,33 @@ app.use( express.static(path.resolve(__dirname, './public')));
 
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'))
+    res.render('home')
 });
 app.get('/register', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'))
+    res.render(path.resolve(__dirname, './views/register.ejs'))
 });
 app.get('/login', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'))
+    res.render(path.resolve(__dirname, './views/login.ejs'))
 });
 app.get('/productCart', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'))
+    res.render(path.resolve(__dirname, './views/productCart.ejs'))
 });
-app.get('/productDetail1', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/productDetail1.html'))
+app.get('/productDetail', (req,res) => {
+    res.render(path.resolve(__dirname, './views/productDetail.ejs'))
 });
 
 app.get('/boardBuilder', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/boardBuilder.html'))
+    res.render(path.resolve(__dirname, './views/boardBuilder.ejs'))
 });
 app.get('/volumeCalculator', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/volumeCalculator.html'))
+    res.render(path.resolve(__dirname, './views/volumeCalculator.ejs'))
 });
 app.get('/shop', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/shop.html'))
+    res.render(path.resolve(__dirname, './views/shop.ejs'))
 });
 
 app.get('/about', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/about.html'))
+    res.render(path.resolve(__dirname, './views/about.ejs'))
 });
 
 app.listen(process.env.PORT || 3000, function () {
