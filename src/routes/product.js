0f8dcb680ee6b38c679331c9842d2dbc5e97e90c
NatsRoit <nats.roitman@gmail.com>
2022-07-19 18:42:36 +0200

    ProductDetail CSS y un poco de EJS

--- src/routes/product.js
+++ src/routes/product.js
@@ -8,30 +8,27 @@ router.get('/',productController.index);
 router.get('/boardBuilder',productController.boardBuilder);
 
 
-router.get('/create',productController.register);
-router.post('/create',productController.create);
-
-;
+// router.get('/create',productController.register);
+// router.post('/create',productController.create);
 
 
-router.get('/login',productController.login);
+// router.get('/login',productController.login);
 //router.post('/login',productController.ingresar);
 
-router.get('/register',productController.register);
-router.post('/register',productController.create);
+// router.get('/register',productController.register);
+// router.post('/register',productController.create);
 
 //ACÁ DEBERIAMOS AGREGAR:  ruta que se activa al momento que el usuario desea salir dela página
 //router.get('/logout', productController.logout)
 
-//ACÁ DEBERIAMOS AGREGAR:  ruta que se activa al momento que el usuario desea salir dela página
-//router.get('/logout', productController.logout);
+
 // router.get('/surfboards',productController.surfboards);
 // router.get('/accesorios',productController.accesorios);
 // router.get('/complementos',productController.complementos);
-//router.get('/cart',productController.cart);
-//router.get('/cart1',productController.cart1);
-//router.get('/cart2',productController.cart2);
-//router.get('/cart3',productController.cart3);
-//router.get('/detail/:id',productController.detail);
+router.get('/cart',productController.cart);
+router.get('/cart1',productController.cart1);
+router.get('/cart2',productController.cart2);
+router.get('/cart3',productController.cart3);
+router.get('/detail/:id',productController.detail);
 
 module.exports = router;
