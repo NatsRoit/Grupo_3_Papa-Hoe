36b7852caa0f162a96b78c39c5dda2f6f3d5d68c
NatsRoit <nats.roitman@gmail.com>
2022-08-24 16:12:09 +0200

    Comento ApiRoutes para HEROKU

--- src/routes/apiUsers.js
+++ src/routes/apiUsers.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const usersApiController = require('../controllers/usersApiController');
+// const express = require('express');
+// const router = express.Router();
+// const usersApiController = require('../controllers/usersApiController');
 
-router.get('/list', usersApiController.list)
-router.get('/detail/:id', usersApiController.detail)
-router.post('/create', usersApiController.create)
-router.get('/delete/:id', usersApiController.destroy)
+// router.get('/list', usersApiController.list)
+// router.get('/detail/:id', usersApiController.detail)
+// router.post('/create', usersApiController.create)
+// router.get('/delete/:id', usersApiController.destroy)
 
 
-module.exports = router;
+// module.exports = router;
