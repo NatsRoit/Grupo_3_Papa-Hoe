1684e20efbfa0cf68d2707fa0ec41559969987a7
NatsRoit <nats.roitman@gmail.com>
2022-08-24 15:18:16 +0200

    Nuevas correcciones para HEROKU

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": 'algotonto',
-    "database": "papahoe",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
     "dialect": "mysql"
   },
   "test": {
