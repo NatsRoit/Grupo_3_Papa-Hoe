c49f481cadb278b8b6404c9beb08d6191c6c3708
¨maleBelaustegui¨ <103225979+maleBelaustegui@users.noreply.github.com>
2022-09-29 16:41:59 -0300

    Ultimos

--- public/js/confirmDelete.js
+++ public/js/confirmDelete.js
@@ -28,13 +28,13 @@ waitForElm('#adm-delete').then((elm) => {
             e.preventDefault();
 
             Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
+                title: 'Estas seguro que queres eliminar el producto?',
+                text: "Esta accion no se puede deshacer!",
                 icon: 'warning',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
+                confirmButtonText: 'Si, eliminar!'
               }).then((result) => {
                 
                 if (result.isConfirmed) {
